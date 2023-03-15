6a56a9e9b14e266dc8cc99dd45f54c8c1f2fadfd
Samiran786 <116157715+Samiran786@users.noreply.github.com>
2023-03-15 12:05:45 +0530

    Add files via upload

--- Java Practice/Pattern_1/src/com/company/pattern_1_recurrsion.java
+++ Java Practice/Pattern_1/src/com/company/pattern_1_recurrsion.java
@@ -6,6 +6,23 @@
 package com.company;
 
 public class pattern_1_recurrsion {
+	public void Pat_1(int n) {
+		if (n<0) {
+			System.out.print("");
+		}else {
+			Pat_1(n-1);
+			for(int j=n;j>0;j--) {
+				System.out.print("* ");
+			}
+			System.out.println();
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		pattern_1_recurrsion p = new pattern_1_recurrsion();
+		p.Pat_1(5);
+	}
 	
 
 }
